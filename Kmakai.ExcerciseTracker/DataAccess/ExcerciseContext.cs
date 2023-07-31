5161c2f4a142c56231299c75cb967a5ccb125cd7
kmakai <makaikwag@gmail.com>
2023-07-31 19:00:52 -0400

    Add project files.

---
+++ Kmakai.ExcerciseTracker/DataAccess/ExcerciseContext.cs
@@ -0,0 +1,16 @@
+﻿using Kmakai.ExcerciseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Kmakai.ExcerciseTracker.DataAccess;
+
+public class ExcerciseContext: DbContext
+{
+    public ExcerciseContext(DbContextOptions<ExcerciseContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Excercise> Excercises { get; set; } = null!;
+
+}
